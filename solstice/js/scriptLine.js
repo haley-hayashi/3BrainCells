ccfd9f7cc90096dfa7143b7a27c95b494d5c30e9
izycisner <iscisner@ucsc.edu>
2019-08-22 21:56:51 -0700

    git commit

---
+++ solstice/js/scriptLine.js
@@ -0,0 +1,61 @@
+function scriptLine(lineNum){
+
+	if(lineNum == 1){
+		return "Great. You’re finally here. You do know what time it is, right?";
+	}
+	if(lineNum == 2){
+		return "*sigh* What am I going to do with you…? You’re late, but I guess it’s okay this time.";
+	}
+	if(lineNum == 3){
+		return "Anyway, you’re here and you need training. That’s what I’m here for. I’ll be your first customer. Let’s get right to it.";
+	}
+	if(lineNum == 4){
+		return "...";
+	}
+	if(lineNum == 5){
+		return "Are you going to ask me for my order or are you just going to stand there?";
+	}
+	if(lineNum == 6){
+		return "So… how do I?";
+	}
+	if(lineNum == 7){
+		return "Alright. First, grab a mug.";
+	}
+	if(lineNum == 8){
+		return "Congrats! You can hold a mug. Now, go ahead and put in ingredient1.";
+	}
+	if(lineNum == 9){
+		return "Well actually we don't have anything to put in the mug..."
+		//return "Now put the mug in the espresso machine and wait for the machine to fill it for you.";
+	}
+	if(lineNum == 10){
+		return "Yeah we don't have those features yet."
+		//return "Alright. Go ahead and put it on the counter and serve it to me. Click that big serve button that only you can see. Don’t ask me how I know, just do it.";
+	}
+	if(lineNum == 11){
+		return "Sorry."
+		//return "Wow! Thanks! You’re so awesome! ...Is what I’d say if I actually cared about what we serve here.";
+	}
+	if(lineNum == 12){
+		return "Well, I guess you can just leave."
+		//return "Alright, now you’re all ready to serve people and not make a total fool outta yourself. Just don’t mess up or I’m docking it from your pay 'cause I’m the manager and I can do that. Now get working.";
+	}
+	if(lineNum == 13){
+		return "The tutorial is over. OVER. O-V-E-R. Finished. Done. Terminado. C’est fini. Go home. Get a life.";
+	}
+	if(lineNum == 14){
+		return "...";
+	}
+	if(lineNum == 15){
+		return "What are you waiting for!? You playtested the game. The game is not done! ";
+	}
+	if(lineNum == 16){
+		return "...";
+	}
+	if(lineNum == 17){
+		return "It means come back later when the useless devs actually have more stuff for you to play.";
+	}
+	if(lineNum == 18){
+		return "Ok I’m shutting this down! Goodbye!";
+	}
+}
