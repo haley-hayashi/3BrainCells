939f44f8890b481cf8ae46796a4d1a43edf68fc2
izycisner <iscisner@ucsc.edu>
2019-08-22 21:51:06 -0700

    git commit

--- solstice/js/main.js
+++ solstice/js/main.js
@@ -158,8 +158,11 @@ Tutorial.prototype = {
 		this.displayText = this.add.text(this.textBox.x + 15, this.textBox.y + 15, "", this.textStyleMain);
 		this.choices = [];
 	},
-	update: function(){
 
+	update: function(){
+		//moves dialogue along
+		if()
+		this.continueStory();
 
 		//movement control of screen
 		if (this.cursors.left.isDown){
@@ -168,7 +171,16 @@ Tutorial.prototype = {
    		if (this.cursors.right.isDown){
         	game.camera.x += 10;
     	}
+	},
+
+	display_Text: function(text){
+		this.displayText.destroy();
+		this.displayText = game.add.text(this.textBox.x + 15, this.textBox.y + 15, "", this.textStyleMain);
+		
+		text = this.parseText(text, this.displayText);
+		this.displayText.setText(text);
 	}
+
 };
 
 //---------------------------------------------------------------------------------------------------------------
